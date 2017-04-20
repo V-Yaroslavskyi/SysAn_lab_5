c1699e46c29cd98f64f20667e1e1ccc67fd5711b
febarabash <ihor.barabash@payrun.org>
2017-04-21 00:49:02 +0300

    baran ne dupli

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,6 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        String style = getClass().getResource("style.css").toExternalForm();
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Lab #5 Yaroslavskyi Doroshchuk Barabash Nogol");
         primaryStage.setScene(new Scene(root));
